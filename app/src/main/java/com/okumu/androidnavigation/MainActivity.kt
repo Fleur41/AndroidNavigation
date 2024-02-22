81c69c39790064521c0edc5416a03be4fa649e1e
Fleur41 <Muriithikings456@gmail.com>
2024-02-22 12:25:06 +0300

    ActionBar was created

--- app/src/main/java/com/okumu/androidnavigation/MainActivity.kt
+++ app/src/main/java/com/okumu/androidnavigation/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.okumu.androidnavigation
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.databinding.DataBindingUtil
 import com.okumu.androidnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //creates an object that is responsible for opening and closing of our drawer
+        val drawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.mainDrawer,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+
+        binding.mainDrawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        //make drawer icon to always appear
+        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
 
         binding.btnSendEmail.setOnClickListener {
             val email = binding.edtEmail.text.toString()
@@ -17,11 +38,11 @@ class MainActivity : AppCompatActivity() {
             // a. explicit intent
             //Logic for navigating to email page
             // 1. Create an intent object
-            val intent = Intent(this, EmailActivity::class.java)
+//            val intent = Intent(this, EmailActivity::class.java)
             // 2. (Optional) Passing data to the next activity(key value format)
-            intent.putExtra("EMAIL", email)
+//            intent.putExtra("EMAIL", email)
             // 3. Start navigating
-            startActivity(intent)
+//            startActivity(intent)
 
 
             // b. Implicit intent
@@ -37,5 +58,33 @@ class MainActivity : AppCompatActivity() {
                 startActivity(implicitIntent)
             }
         }
+    }
+
+     // logic for creation of the menu
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if(menu is MenuBuilder){
+            menu.setOptionalIconsVisible(true)
+        }
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    //add functionality to the menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_settings -> {
+                //show toast message
+                Toast.makeText(
+                    this,
+                    "You have clicked settings",
+                    Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_exit -> {
+                //close activity
+                finish()
+            }
+        }
+        return true
     }
 }
