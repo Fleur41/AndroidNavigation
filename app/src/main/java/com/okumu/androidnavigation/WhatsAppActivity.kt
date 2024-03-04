76cb63acd766d01f326a4fb1bd48fadde94c42b5
Fleur41 <Muriithikings456@gmail.com>
2024-03-04 12:16:44 +0300

    different functions are created i.e onCreate, onStart, onResume, onPause, onStop,onDestroy

--- app/src/main/java/com/okumu/androidnavigation/WhatsAppActivity.kt
+++ app/src/main/java/com/okumu/androidnavigation/WhatsAppActivity.kt
@@ -2,6 +2,7 @@ package com.okumu.androidnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -10,6 +11,7 @@ import com.okumu.androidnavigation.databinding.ActivityWhatsAppBinding
 class WhatsAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //toast("onCreate called")
         val binding: ActivityWhatsAppBinding = DataBindingUtil.setContentView(this, R.layout.activity_whats_app)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment
@@ -28,4 +30,35 @@ class WhatsAppActivity : AppCompatActivity() {
             navController.navigate(R.id.callsFragment)
         }
     }
+
+    
+    override fun onStart(){
+        super.onStart()
+        toast("On Start Created")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        toast("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        toast("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        toast("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        toast("onDestroy Called")
+    }
+    //utility function for showing a toast message
+    private fun toast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
 }
+
